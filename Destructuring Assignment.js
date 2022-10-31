e0b277d4911ea334858e912e1ea51a58292c41a6
Hasibullah Aman <hasib.aman7@gmail.com>
2022-10-31 04:58:25 +0430

    02

--- Destructuring Assignment.js
+++ Destructuring Assignment.js
@@ -1,3 +1,8 @@
+// Destructuring Assignment
+// This method say insted of that which we use like
+//     Name = myob.name or Father_name = myob.name 
+//     we can use const{name,Father_name} = myob
+// this method use for extract data from Object.
 const myob = {
     name: "Hasibullah",
     Father_Name: 'Ghullam Hassan'
@@ -5,3 +10,6 @@ const myob = {
 
 console.log(myob.name)
 console.log(myob.Father_Name)
+
+// const { name, Father_Name } = myob;
+// console.log("my Name is: " + name + "and my Father name is: " + Father_Name)
